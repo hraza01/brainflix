adfcb42a8108062cd667ace5cce5233be95bba20
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-02-16 22:52:55 -0500

    feat(pages): added upload videos page & styles

--- src/components/NavBar/UploadButton.jsx
+++ src/components/NavBar/UploadButton.jsx
@@ -1,15 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Link } from 'react-router-dom';
 import React from 'react';
 import UploadIcon from '../../assets/icons/upload.svg';
 import './_NavBar.scss';
 
 function UploadButton() {
   return (
-    <div className="nav__btn-container">
+    <Link className="nav__btn-container" to="/upload-video">
       <button type="button" className="btn nav__btn">
         <img className="nav__btn-logo" src={UploadIcon} alt="search-logo" />
         <span className="nav__btn-text">upload</span>
       </button>
-    </div>
+    </Link>
   );
 }
 
